52b4c32716006c09a617d5447cd9ce24f0784b1c
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2020-08-31 09:49:37 -0400

    Fixed urlscan paging.

--- providers/urlscan_provider.py
+++ providers/urlscan_provider.py
@@ -1,18 +1,24 @@
-from providers.base_provider import BaseProvider
+from urllib.parse import urlparse
 import requests
 from dateutil import parser as date_parser
-from urllib.parse import urlparse
+from providers.base_provider import BaseProvider
 
 
 class UrlScanProvider(BaseProvider):
+    request_size = 1000
+
     def __init__(self, ignored_extensions):
         super(UrlScanProvider, self).__init__(ignored_extensions, False)  # Results are already sorted.
 
     def fetch_internal(self, domain):
-        offset = 0
-
+        params = {
+            "q": f"domain:{domain}",
+            "sort_field": "date",
+            "sort_order": "desc",
+            "size": self.request_size
+        }
         while True:
-            results = requests.get("https://urlscan.io/api/v1/search/?q=domain%3A%22" + domain + "%22&offset=" + str(offset) + "&sort_field=date&sort_order=desc").json()["results"]
+            results = requests.get("https://urlscan.io/api/v1/search/", params=params).json()["results"]
             for result in results:
                 url = result["page"]["url"]
                 time = date_parser.parse(result["task"]["time"])
@@ -23,7 +29,7 @@ class UrlScanProvider(BaseProvider):
                     if domain in urlparse(url).netloc:
                         yield time, url
 
-            if len(results) < 100:
+            if len(results) < self.request_size:
                 break
 
-            offset += 100
+            params["search_after"] = ",".join(str(v) for v in results[-1]["sort"])
